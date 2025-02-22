d384f706a919fae0f32e549254c44e32bcea52fc
1028ouo <shellyyi1028@gmail.com>
2025-02-23 03:52:47 +0800

    修正測試以使用 MyApp 實例，並更新 README.md 待辦事項，新增資源和字型設定

--- README.md
+++ README.md
@@ -14,3 +14,12 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+
+## 待辦事項
+1. 修改所有按鈕樣式
+2. 加入音效
+~~3. 使方塊生成變成完全隨機~~
+~~4. 讓主畫面的得分不要一開始就顯示~~
+5. 記錄最佳紀錄
+~~6. 遊戲結束的呈現畫面~~
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,31 +18,56 @@ class MyApp extends StatelessWidget {
 }
 
 class GamePage extends StatefulWidget {
+  const GamePage({super.key});
+
   @override
   _GamePageState createState() => _GamePageState();
 }
 
 class _GamePageState extends State<GamePage> {
+  // 常數定義
+  static const double BLOCK_HEIGHT = 125.0;
+  static const double BLOCK_WIDTH = 125.0;
+  static const int INITIAL_TIME = 10;
+  static const int INITIAL_COUNTDOWN = 3;
+
+  // 通用樣式
+  static const TextStyle commonTextStyle = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Crayon',
+  );
+
+  static const Shadow commonShadow = Shadow(
+    offset: Offset(5, 5),
+    blurRadius: 5,
+    color: Color.fromARGB(255, 62, 50, 50),
+  );
+
+  // 狀態變數
   int score = 0;
   Timer? gameTimer;
   Timer? dropTimer;
-  Random random = Random();
-  List<List<int>> grid = [
-    [0, 1, 0],
-    [1, 0, 0],
-    [1, 0, 0],
-    [0, 1, 0],
-    [0, 0, 1],
-    [2, 2, 2]
-  ]; // 初始方塊位置
+  Timer? countdownTimer;
+  final random = Random();
+  late List<List<int>> grid;
   bool isGameOver = false;
   bool isGameStarted = false;
-  final double blockHeight = 50.0; // 方塊高度
-  int remainingTime = 60; // 剩餘時間
+  int remainingTime = INITIAL_TIME;
+  int countdown = INITIAL_COUNTDOWN;
+  bool isPaused = false;
 
   @override
   void initState() {
     super.initState();
+  }
+
+  List<List<int>> generateRandomGrid() {
+    List<List<int>> newGrid = List.generate(6, (i) => List.filled(3, 0));
+    for (int i = 0; i < 5; i++) {
+      newGrid[i][random.nextInt(3)] = 1;
+    }
+    newGrid[5] = [2, 2, 2];
+    return newGrid;
   }
 
   void startGame() {
@@ -48,18 +75,28 @@ class _GamePageState extends State<GamePage> {
       isGameStarted = true;
       score = 0;
       isGameOver = false;
-      remainingTime = 60;
-      grid = [
-        [0, 1, 0],
-        [1, 0, 0],
-        [1, 0, 0],
-        [0, 1, 0],
-        [0, 0, 1],
-        [2, 2, 2]
-      ]; // 初始化方塊位置
+      remainingTime = INITIAL_TIME;
+      countdown = INITIAL_COUNTDOWN;
+      grid = generateRandomGrid();
     });
 
-    gameTimer = Timer.periodic(Duration(seconds: 1), (timer) {
+    countdownTimer = Timer.periodic(
+      const Duration(seconds: 1),
+      (timer) {
+        setState(() {
+          if (countdown > 0) {
+            countdown--;
+          } else {
+            timer.cancel();
+            startMainGame();
+          }
+        });
+      },
+    );
+  }
+
+  void startMainGame() {
+    gameTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
       setState(() {
         if (remainingTime > 0) {
           remainingTime--;
@@ -67,22 +104,7 @@ class _GamePageState extends State<GamePage> {
           isGameOver = true;
           timer.cancel();
           dropTimer?.cancel();
-          showDialog(
-            context: context,
-            builder: (context) => AlertDialog(
-              title: Text('遊戲結束'),
-              content: Text('你在一分鐘內消除了 $score 個方塊'),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                    startGame();
-                  },
-                  child: Text('再來一次'),
-                ),
-              ],
-            ),
-          );
+          setState(() {}); // 觸發重繪以顯示遊戲結束畫面
         }
       });
     });
@@ -93,11 +115,7 @@ class _GamePageState extends State<GamePage> {
       if (grid[4][column] == 1) {
         grid[4][column] = 0;
         score++;
-        // 隨機生成新的方塊位置，確保每一列只有一個方塊
         int newColumn = random.nextInt(3);
-        while (grid[0][newColumn] == 1) {
-          newColumn = random.nextInt(3);
-        }
         grid.insert(0, [0, 0, 0]);
         grid[0][newColumn] = 1;
         grid.removeLast();
@@ -105,81 +123,315 @@ class _GamePageState extends State<GamePage> {
     });
   }
 
+  void togglePause() {
+    setState(() {
+      isPaused = !isPaused;
+      if (isPaused) {
+        gameTimer?.cancel();
+        countdownTimer?.cancel();
+      } else {
+        startMainGame();
+      }
+    });
+  }
+
+  void restartGame() {
+    setState(() {
+      isPaused = false;
+      isGameStarted = false;
+      startGame();
+    });
+  }
+
   @override
   void dispose() {
     gameTimer?.cancel();
     dropTimer?.cancel();
+    countdownTimer?.cancel();
     super.dispose();
+  }
+
+  Widget buildGameControls() {
+    return Row(
+      children: List.generate(
+        3,
+        (index) => Expanded(
+          child: GestureDetector(
+            onTap: () => onButtonPressed(index),
+            child: Image.asset(
+              'assets/star.png',
+              fit: BoxFit.cover,
+              height: 90,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildPauseOverlay() {
+    return Positioned.fill(
+      child: Container(
+        color: Colors.black.withOpacity(0.5),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                '暫停',
+                style: commonTextStyle.copyWith(
+                  fontSize: 60,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 20),
+              for (var buttonData in [
+                {'text': '継続', 'onPressed': togglePause},
+                {'text': '重新', 'onPressed': restartGame},
+              ])
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: SizedBox(
+                    width: 200,
+                    child: ElevatedButton(
+                      onPressed: buttonData['onPressed'] as void Function(),
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 50,
+                          vertical: 20,
+                        ),
+                        textStyle: commonTextStyle.copyWith(fontSize: 30),
+                      ),
+                      child: Text(buttonData['text'] as String),
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildGameOverOverlay() {
+    return Positioned.fill(
+      child: Container(
+        color: Colors.black.withOpacity(0.5),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  style: commonTextStyle.copyWith(fontSize: 70),
+                  children: [
+                    TextSpan(
+                      text: 'GAME',
+                      style: TextStyle(
+                        color: const Color.fromARGB(255, 111, 166, 255),
+                        shadows: const [commonShadow],
+                      ),
+                    ),
+                    TextSpan(
+                      text: ' OVER!',
+                      style: TextStyle(
+                        color: const Color.fromARGB(255, 255, 101, 91),
+                        shadows: const [commonShadow],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  style: commonTextStyle.copyWith(fontSize: 50),
+                  children: [
+                    TextSpan(
+                      text: 'score: ',
+                      style: TextStyle(
+                        color: const Color.fromARGB(255, 220, 220, 220),
+                        shadows: const [commonShadow],
+                      ),
+                    ),
+                    TextSpan(
+                      text: '$score',
+                      style: TextStyle(
+                        color: const Color.fromARGB(255, 255, 234, 0),
+                        shadows: const [commonShadow],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      isGameOver = false;
+                      startGame();
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 50,
+                      vertical: 20,
+                    ),
+                    textStyle: commonTextStyle.copyWith(fontSize: 35),
+                  ),
+                  child: const Text('AGAIN'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('打方塊遊戲'),
-      ),
-      body: Column(
+      body: Stack(
         children: [
-          if (isGameStarted)
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Align(
-                alignment: Alignment.topLeft,
-                child: Text('剩餘時間: $remainingTime 秒'),
+          Positioned.fill(
+            child: Image.asset(
+              isGameStarted
+                  ? 'assets/inside_background.png'
+                  : 'assets/main_background.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Column(
+            children: [
+              if (isGameStarted && countdown > 0)
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      '準備\n$countdown',
+                      textAlign: TextAlign.center,
+                      style: commonTextStyle.copyWith(
+                        fontSize: 60,
+                        color: const Color.fromARGB(255, 246, 115, 106),
+                        shadows: const [commonShadow],
+                      ),
+                    ),
+                  ),
+                ),
+              if (isGameStarted && countdown == 0)
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(8.0, 50.0, 8.0, 8.0),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      '時間: $remainingTime 秒',
+                      style: commonTextStyle.copyWith(fontSize: 20),
+                    ),
+                  ),
+                ),
+              if (!isPaused && isGameStarted) buildGameGrid(),
+              if (!isGameStarted) buildMainMenu(),
+              if (isGameStarted && countdown == 0 && !isPaused) ...[
+                buildGameControls(),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 17.0),
+                  child: Text(
+                    '得分: $score',
+                    style: commonTextStyle.copyWith(fontSize: 24),
+                  ),
+                ),
+              ],
+            ],
+          ),
+          if (isGameStarted && countdown == 0)
+            Positioned(
+              top: 40,
+              right: 10,
+              child: IconButton(
+                icon: Icon(isPaused ? Icons.play_arrow : Icons.pause),
+                color: Colors.white,
+                iconSize: 30,
+                onPressed: togglePause,
               ),
             ),
-          Expanded(
-            child: Stack(
-              children: [
-                if (isGameStarted)
-                  for (int i = 0; i < 5; i++)
-                    for (int j = 0; j < 3; j++)
-                      if (grid[i][j] == 1)
-                        Positioned(
-                          top: MediaQuery.of(context).size.height / 6 * i,
-                          left: MediaQuery.of(context).size.width / 3 * j,
-                          child: Container(
-                            width: MediaQuery.of(context).size.width / 3,
-                            height: blockHeight,
-                            color: Colors.red,
-                          ),
-                        ),
-              ],
+          if (isPaused) buildPauseOverlay(),
+          if (isGameOver) buildGameOverOverlay(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildGameGrid() {
+    return Expanded(
+      child: Stack(
+        children: [
+          if (isGameStarted && countdown == 0)
+            for (int i = 0; i < 5; i++)
+              for (int j = 0; j < 3; j++)
+                if (grid[i][j] == 1)
+                  Positioned(
+                    top: BLOCK_HEIGHT * i,
+                    left: BLOCK_WIDTH * j,
+                    child: SizedBox(
+                      width: BLOCK_WIDTH,
+                      height: BLOCK_HEIGHT,
+                      child: Image.asset(
+                        'assets/kirby01.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildMainMenu() {
+    return Expanded(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                style: commonTextStyle.copyWith(fontSize: 50),
+                children: [
+                  TextSpan(
+                    text: 'カービィ\n踏 ',
+                    style: TextStyle(
+                      color: const Color.fromARGB(255, 255, 140, 140),
+                      shadows: const [commonShadow],
+                    ),
+                  ),
+                  TextSpan(
+                    text: '星 星',
+                    style: TextStyle(
+                      color: Colors.yellow,
+                      shadows: const [commonShadow],
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          if (!isGameStarted)
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: startGame,
-              child: Text('開始遊戲'),
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 50,
+                  vertical: 20,
+                ),
+                textStyle: commonTextStyle.copyWith(fontSize: 30),
+              ),
+              child: const Text('START！'),
             ),
-          if (isGameStarted)
-            Row(
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () => onButtonPressed(0),
-                    child: Text('按鈕 1'),
-                  ),
-                ),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () => onButtonPressed(1),
-                    child: Text('按鈕 2'),
-                  ),
-                ),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () => onButtonPressed(2),
-                    child: Text('按鈕 3'),
-                  ),
-                ),
-              ],
-            ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text('得分: $score'),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
